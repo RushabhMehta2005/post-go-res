56222c332551cf42ac3bf43399af53988ab3e6c4
Rushabh Mehta <mehtarushabh2005@gmail.com>
2025-09-05 16:11:12 +0530

    Handle invalid length commands, use bufio.Writer instead of directly writing using conn.Write Tradeoff: Have to call writer.Flush() to actually send the message to the client.

--- main.go
+++ main.go
@@ -58,25 +58,54 @@ func handleConnection(conn net.Conn) {
 	fmt.Println("Connection established by client: ", conn.RemoteAddr().String())
 	fmt.Println("Concurrent clients: ", concurrent_clients)
 
-	scanner := bufio.NewScanner(conn)
+	reader := bufio.NewScanner(conn)
+	writer := bufio.NewWriter(conn)
+
 	for {
-		next := scanner.Scan()
+		next := reader.Scan()
 		if next {
-			cmd_raw_string := scanner.Text()
+			cmd_raw_string := reader.Text()
 			cmd_fields := strings.Fields(cmd_raw_string)
+
+			// Skip empty newlines by client, QOL change
+			if len(cmd_fields) == 0 {
+				continue
+			}
+
 			switch cmd_fields[0] {
-			case "SET": // length better be 3
-				store[cmd_fields[1]] = cmd_fields[2]
-				conn.Write([]byte("SET OK: Wrote value of " + cmd_fields[1] + " as " + cmd_fields[2] + "\n"))
-			case "GET": // length better be 2
-				val, ok := store[cmd_fields[1]]
-				if ok {
-					conn.Write([]byte("GET OK: Got value of " + cmd_fields[1] + " as " + val + "\n"))
-				} else {
-					conn.Write([]byte("GET NOT OK: Did not find " + cmd_fields[1] + " in store" + "\n"))
+			case "SET":
+				switch len(cmd_fields) {
+				case 1:
+					writer.WriteString("SET NOT OK: No key or value provided\n")
+				case 2:
+					writer.WriteString("SET NOT OK: No value provided\n")
+				case 3:
+					// Happy flow
+					store[cmd_fields[1]] = cmd_fields[2]
+					writer.WriteString("SET OK: Wrote value of " + cmd_fields[1] + " as " + cmd_fields[2] + "\n")
+				default:
+					writer.WriteString("SET NOT OK: Invalid arguments\n")
+				}
+			case "GET":
+				switch len(cmd_fields) {
+				case 1:
+					writer.WriteString("GET NOT OK: No key provided\n")
+				case 2:
+					val, found := store[cmd_fields[1]]
+					if found {
+						writer.WriteString("GET OK: Got value of " + cmd_fields[1] + " as " + val + "\n")
+					} else {
+						writer.WriteString("GET NOT OK: Did not find " + cmd_fields[1] + " in store" + "\n")
+					}
+				default:
+					writer.WriteString("GET NOT OK: Invalid arguments\n")
 				}
 			default:
-				conn.Write([]byte("INVALID COMMAND" + "\n"))
+				writer.WriteString("INVALID COMMAND" + "\n")
+			}
+			err := writer.Flush()
+			if err != nil {
+				fmt.Println("Could not flush data to client: ", conn.RemoteAddr())
 			}
 
 			// Only to see
