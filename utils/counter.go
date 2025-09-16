74a9f0046717dba9831219eab0f0f947602e51f6
Rushabh Mehta <mehtarushabh2005@gmail.com>
2025-09-16 21:22:08 +0530

    Design an implement an atomic counter struct, part of server state refactor.

---
+++ utils/counter.go
@@ -0,0 +1,27 @@
+package utils
+
+import "sync/atomic"
+
+type AtomicCounter struct {
+	c int64
+}
+
+func NewCounter() (*AtomicCounter) {
+	return &AtomicCounter{}
+}
+
+func (counter *AtomicCounter) Add(delta int64) {
+    atomic.AddInt64(&counter.c, delta)
+}
+
+func (counter *AtomicCounter) Inc() {
+	atomic.AddInt64(&counter.c, 1)
+}
+
+func (counter *AtomicCounter) Dec() {
+	atomic.AddInt64(&counter.c, -1)
+}
+
+func (counter *AtomicCounter) Read() (int64) {
+	return counter.c
+}
