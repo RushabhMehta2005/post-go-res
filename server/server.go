19f95774cf09f100ddb9149fddc297cb47920012
Rushabh Mehta <mehtarushabh2005@gmail.com>
2025-09-18 19:32:59 +0530

    Refactor handleConnection method on Server struct, clean up spaghetti code

--- server/server.go
+++ server/server.go
@@ -12,18 +12,18 @@ import (
 )
 
 type Server struct {
-	kvstore store.InMemStore // Actual in memory store
-	walHandler *wal.WAL // Write Ahead Logger
+	kvstore           store.InMemStore    // Actual in memory store
+	walHandler        *wal.WAL            // Write Ahead Logger
 	concurrentClients utils.AtomicCounter // The number of clients connected right now
-	port string // The default port on which our application will run
+	port              string              // The default port on which our application will run
 }
 
-func NewServer(kvstore store.InMemStore, walHandler *wal.WAL) (*Server) {
+func NewServer(kvstore store.InMemStore, walHandler *wal.WAL) *Server {
 	return &Server{
-		kvstore: kvstore,
-		walHandler: walHandler,
+		kvstore:           kvstore,
+		walHandler:        walHandler,
 		concurrentClients: utils.AtomicCounter{},
-		port: "4242",
+		port:              "4242",
 	}
 }
 
@@ -34,7 +34,7 @@ func (s *Server) Start() {
 	s.walHandler.ReBuild(s.kvstore)
 
 	// Listen for tcp connections on the port
-	listener, err := net.Listen("tcp", ":" + s.port)
+	listener, err := net.Listen("tcp", ":"+s.port)
 
 	if err != nil {
 		log.Fatal("Could not listen on port", s.port)
@@ -52,14 +52,11 @@ func (s *Server) Start() {
 			log.Println("Could not accept connection from a client.")
 		}
 
-		s.concurrentClients.Inc()
-
 		// Launch a goroutine to handle the connection, allowing main loop to accept more connections
 		go s.handleConnection(conn)
 	}
 }
 
-// TODO: Refactor this function
 
 func (s *Server) handleConnection(conn net.Conn) {
 	// No matter how we end up handling this connection, always close the connection and decrease number of clients
@@ -72,62 +69,64 @@ func (s *Server) handleConnection(conn net.Conn) {
 		log.Println("Concurrent clients:", s.concurrentClients.Read())
 	}()
 
+	s.concurrentClients.Inc()
+
 	log.Println("Connection established by client:", conn.RemoteAddr().String())
 	log.Println("Concurrent clients:", s.concurrentClients.Read())
 
-	reader := bufio.NewScanner(conn)
+	scanner := bufio.NewScanner(conn)
 	writer := bufio.NewWriter(conn)
 
-	for {
-		next := reader.Scan()
-		if next {
-			cmd_raw_string := reader.Text()
-			cmd_fields := strings.Fields(cmd_raw_string)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Fields(line)
 
-			// Skip empty newlines by client, QOL change
-			if len(cmd_fields) == 0 {
-				continue
-			}
+		// Skip empty newlines by client, QOL change
+		if len(parts) == 0 {
+			continue
+		}
 
-			switch cmd_fields[0] {
-			case "SET":
-				switch len(cmd_fields) {
-				case 1:
-					writer.WriteString("SET NOT OK: No key or value provided\n")
-				case 2:
-					writer.WriteString("SET NOT OK: No value provided\n")
-				case 3:
-					s.walHandler.Log(cmd_fields[0], cmd_fields[1], cmd_fields[2])
-					s.kvstore.Set(cmd_fields[1], cmd_fields[2])
-					writer.WriteString("SET OK: Wrote value of " + cmd_fields[1] + " as " + cmd_fields[2] + "\n")
-				default:
-					writer.WriteString("SET NOT OK: Invalid arguments\n")
-				}
-			case "GET":
-				switch len(cmd_fields) {
-				case 1:
-					writer.WriteString("GET NOT OK: No key provided\n")
-				case 2:
-					val, found := s.kvstore.Get(cmd_fields[1])
-					if found {
-						writer.WriteString("GET OK: Got value of " + cmd_fields[1] + " as " + val + "\n")
-					} else {
-						writer.WriteString("GET NOT OK: Did not find " + cmd_fields[1] + " in store" + "\n")
-					}
-				default:
-					writer.WriteString("GET NOT OK: Invalid arguments\n")
-				}
-			default:
-				writer.WriteString("INVALID COMMAND" + "\n")
-			}
-			err := writer.Flush()
-			if err != nil {
-				log.Println("Could not flush data to client:", conn.RemoteAddr())
-			}
+		command := parts[0]
+		args := parts[1:]
 
-		} else {
-			break
+		var response string
+
+		switch command {
+		case "SET":
+			response = s.handleSet(args)
+		case "GET":
+			response = s.handleGet(args)
+		default:
+			response = "INVALID COMMAND\n"
+		}
+
+		writer.WriteString(response)
+		err := writer.Flush()
+		if err != nil {
+			log.Println("Could not flush data to client:", conn.RemoteAddr())
 		}
 	}
 
+}
+
+func (s *Server) handleSet(args []string) string {
+	if len(args) != 2 {
+		return "SET NOT OK: Expected 2 arguments KEY and VALUE\n"
+	}
+	key, value := args[0], args[1]
+	s.walHandler.Log("SET", key, value)
+	s.kvstore.Set(key, value)
+	return "SET OK: Wrote value of " + key + " as " + value + "\n"
+}
+
+func (s *Server) handleGet(args []string) string {
+	if len(args) != 1 {
+		return "GET NOT OK: Expected 1 argument KEY\n"
+	}
+	key := args[0]
+	value, ok := s.kvstore.Get(key)
+	if !ok {
+		return "GET NOT OK: Could not find " + key + " in store\n"
+	}
+	return "GET OK: Got value of " + key + " as " + value + "\n"
 }
