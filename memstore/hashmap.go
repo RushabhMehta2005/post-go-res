84ae20251da3e000ce785499dc8976c25ebd83e8
Rushabh Mehta <mehtarushabh2005@gmail.com>
2025-09-20 20:03:06 +0530

    Rename rwMutex in HashMap struct to mu

--- memstore/hashmap.go
+++ memstore/hashmap.go
@@ -5,7 +5,7 @@ import "sync"
 // The 'HashMap' struct implements a thread safe in memory store using
 // the map data structure in Go
 type HashMap struct {
-	rwMutex sync.RWMutex
+	mu sync.RWMutex
 	data    map[string]string
 }
 
@@ -18,22 +18,22 @@ func NewHashMap(initialSize int) *HashMap {
 // Gets value from map given the key.
 // If key does not exist in map, false is returned alongside the empty string
 func (s *HashMap) Get(key string) (string, bool) {
-	s.rwMutex.RLock()
-	defer s.rwMutex.RUnlock()
+	s.mu.RLock()
+	defer s.mu.RUnlock()
 	val, found := s.data[key]
 	return val, found
 }
 
 // Set key to value in map
 func (s *HashMap) Set(key, value string) {
-	s.rwMutex.Lock()
-	defer s.rwMutex.Unlock()
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	s.data[key] = value
 }
 
 // Delete key from map
 func (s *HashMap) Delete(key string) {
-	s.rwMutex.Lock()
-	defer s.rwMutex.Unlock()
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	delete(s.data, key)
 }
